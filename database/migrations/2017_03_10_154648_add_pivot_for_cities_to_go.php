e1ca6eda8cf948c8007742154737b997024a9713
Olexiy <shevchuka@gmail.com>
2017-03-20 16:27:42 +0200

    add city sa possible to go by default

--- database/migrations/2017_03_10_154648_add_pivot_for_cities_to_go.php
+++ database/migrations/2017_03_10_154648_add_pivot_for_cities_to_go.php
@@ -14,7 +14,7 @@ class AddPivotForCitiesToGo extends Migration
     public function up()
     {
         Schema::table('city_city_to_go', function (Blueprint $table) {
-            $table->tinyInteger('is_possible_to_get')->default(0);
+            $table->tinyInteger('is_possible_to_get')->default(1);
             $table->integer('weight')->default(0);
         });
     }
